750d0db544185ab8ee63bfb95c45d8c056869cc0
rayinianji <47687829+rayinianji@users.noreply.github.com>
2019-09-14 11:19:46 +0530

    Update second.groovy

--- second.groovy
+++ second.groovy
@@ -1,4 +1,12 @@
 freeStyleJob("Myfirst_job")
 {
   description('My first job')
+      scm {
+        git {
+            remote {
+                name('origin')
+                url('https://github.com/rayinianji/Programs.git')
+            }
+        }
+      }
 }
