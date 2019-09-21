a32e23faf3c12d5176922c341c53ae70694cb4f7
rayinianji <anjaneyulu195@gmail.com>
2019-09-21 17:35:36 +0530

    added

--- Fstyle.groovy
+++ Fstyle.groovy
@@ -1,12 +1,21 @@
-freeStyleJob("Fstyelejob_gitconfig")
+freeStyleJob("Foldercreation")
 {
   description('My first job_SSH')
       scm {
         git {
             remote {
                 name('origin')
-                url('git@github.com:rayinianji/Programs.git')
+                url('https://github.com/rayinianji/Programs.git')
             }
         }
       }
+  steps {
+  dsl {
+            //external('projectA.groovy', 'projectB.groovy')
+            external('foldercreation.groovy')
+            removeAction('DISABLE')
+            ignoreExisting()
+            //additionalClasspath('lib')
+        }
+  }
 }
