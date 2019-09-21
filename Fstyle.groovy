fba0f86e41d94057f1efd64efb594c1da798ef05
rayinianji <anjaneyulu195@gmail.com>
2019-09-21 17:16:21 +0530

    added new one

--- Fstyle.groovy
+++ Fstyle.groovy
@@ -1,1 +1,12 @@
-freeStyleJob('FirstFstyle')
+freeStyleJob("Fstyelejob_gitconfig")
+{
+  description('My first job_SSH')
+      scm {
+        git {
+            remote {
+                name('origin')
+                url('git@github.com:rayinianji/Programs.git')
+            }
+        }
+      }
+}
