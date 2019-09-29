f39e76edbd568f4f96df3ca212ea436361020ee8
rayinianji <47687829+rayinianji@users.noreply.github.com>
2019-09-29 23:16:21 +0530

    Update firstPline.groovy

--- firstPline.groovy
+++ firstPline.groovy
@@ -1,4 +1,15 @@
-pipelineJob('MyFirstPipeLine') {
+folder ("$ProductName")
+{}
+folder ("$ProductName/$AppName")
+{}
+pipelineJob("$ProductName/$AppName/CICD-$AppName") {
+    parameters {
+        
+		stringParam('ProductName',"$ProductName", "ProductName")
+        stringParam('AppName',"$AppName", "Application Name")
+        
+    }
+    
     definition {
         cps {
             script(readFileFromWorkspace('CI.groovy'))
