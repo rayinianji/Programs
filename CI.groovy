f190416b9084a80b7b30a94db689673857a12ef9
rayinianji <47687829+rayinianji@users.noreply.github.com>
2019-10-08 09:07:44 +0530

    Update CI.groovy

--- CI.groovy
+++ CI.groovy
@@ -24,6 +24,7 @@ wrks = env.WORKSPACE
    {
      println(" Cloning--------------- ")
 	load 'cln.groovy'
+     println(" Cloning--------------- end...........")	   
    }
      stage ('Build')
    {
